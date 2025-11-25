86224a3e7285394d829f9952f37945d6e171040d
arkzam13 <arkadiuszzamojski13@gmail.com>
2025-11-25 10:56:00 +0100

    dynamiczne rozmiary
    dodane dynamiczne rozmiary planszy

--- PathfindingFullStack/pathfindingfullstack.client/src/App.tsx
+++ PathfindingFullStack/pathfindingfullstack.client/src/App.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 
 function App() {
-    return (
-        <div id="duzyBen">
-            <Board height={10} width={10}>
-            </Board>
-        </div>
-    );
+  const [height, setHeight] = useState(10);
+  const [width, setWidth] = useState(10);
+return (
+    <>
+      <div>wymiary
+        <input type="number"
+              min={10}
+              value={height}
+              onChange={(e) => setHeight(Number(e.target.value))}/>
+        <input type="number"
+              min={10}
+              value={width}
+              onChange={(e) => setWidth(Number(e.target.value))} />
+      </div>
+      <div id="duzyBen">
+        <Board height={height} width={width}>
+        </Board>
+      </div>
+    </>
+  );
 }
 
 export default App;
